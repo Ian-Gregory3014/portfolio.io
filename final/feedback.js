250b36f2a5759ea111602374cac447a6199042b8
Ian-Gregory3014 <93553058+Ian-Gregory3014@users.noreply.github.com>
2023-03-03 16:15:31 -0600

    Add files via upload

---
+++ final/feedback.js
@@ -0,0 +1,50 @@
+function feedbackFormValidate()
+{
+    var contactFormObj = document.getElementById("contactForm");
+    var email = contactFormObj.email.value;
+    var everythingOK = true;
+    
+    if (!validateEmail(email))
+    {
+        alert("Error: Invalid e-mail address.");
+        everythingOK = false;
+    }
+    
+    if (everythingOK)
+    {
+        if (contactFormObj.reply.checked)
+            alert("Warning: The e-mail feature is currently not supported.");
+        alert("All the information looks good.\nThank you!");
+        return true;
+    }
+    else
+        return false;
+}
+
+function validateName(name)
+{
+    var p = name.search(/^[-'\w\s]+$/);
+    if (p == 0)
+        return true;
+    else
+        return false;
+}
+
+function validatePhone(phone)
+{
+    var p1 = phone.search(/^\d{3}[-\s]{0,1}\d{3}[-\s]{0,1}\d{4}$/);
+    var p2 = phone.search(/^\d{3}[-\s]{0,1}\d{4}$/);
+    if (p1 == 0 || p2 == 0)
+        return true;
+    else
+        return false;
+}
+
+function validateEmail(address)
+{
+    var p = address.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/);
+    if (p == 0)
+        return true;
+    else
+        return false;
+}
